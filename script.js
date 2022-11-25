4400edeeeec455b24ef4532afbaf264c8da3c881
vnrydx <vnrydx@gmail.com>
2022-11-25 17:25:57 -0600

    added reverse button in HTML and made it function in JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+
+const reverseButton = document.getElementById("reverse-button");
+
+function clickTransformScaleSwap() {
+    if (document.body.style.transform === "") {
+        document.body.style.transform = "scaleX(-1)";
+    }
+    else if (document.body.style.transform === "scaleX(-1)") {
+        document.body.style.transform = "scaleX(1)";
+    }
+    else if (document.body.style.transform === "scaleX(1)") {
+        document.body.style.transform = "scaleX(-1)";
+    }
+}
+
+reverseButton.addEventListener("click", clickTransformScaleSwap);
